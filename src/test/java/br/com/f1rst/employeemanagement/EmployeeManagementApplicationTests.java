7785c41920cb98c2ad5e8a6e98f65671694eb937
phellipe <phelliperrp@gmail.com>
2025-08-08 15:00:56 -0300

    init

---
+++ src/test/java/br/com/f1rst/employeemanagement/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.f1rst.employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
