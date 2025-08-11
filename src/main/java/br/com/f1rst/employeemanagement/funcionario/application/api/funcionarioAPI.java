99989f7741a74b40a7a0d495e7b7c149205d00c8
phellipe <phelliperrp@gmail.com>
2025-08-11 15:52:20 -0300

    adiciona context path no yml

--- src/main/java/br/com/f1rst/employeemanagement/funcionario/application/api/funcionarioAPI.java
+++ src/main/java/br/com/f1rst/employeemanagement/funcionario/application/api/funcionarioAPI.java
@@ -1,4 +1,9 @@
 package br.com.f1rst.employeemanagement.funcionario.application.api;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/funcionario")
 public interface funcionarioAPI {
 }
