b2df7944c948da416f3d15feeb9a2146ba9dc8b7
phellipe <phelliperrp@gmail.com>
2025-08-12 17:54:38 -0300

    implementa camada repository

--- src/main/java/br/com/f1rst/employeemanagement/funcionario/infra/FuncionarioInfraRepository.java
+++ src/main/java/br/com/f1rst/employeemanagement/funcionario/infra/FuncionarioInfraRepository.java
@@ -45,6 +45,7 @@ public class FuncionarioInfraRepository implements FuncionarioRepository {
     @Override
     public void deletaFuncionario(Funcionario funcionario) {
         log.info("[inicia] FuncionarioInfraRepository - deletaFuncionario");
+        funcionarioSpringDataJPARepository.delete(funcionario);
         log.info("[finaliza] FuncionarioInfraRepository - deletaFuncionario");
     }
 }
